ec25e4f03ad16c2aaa5d025c1c9144f2ec3bf37f
MiChong <1564666023@qq.com>
2017-02-20 20:41:23 +0800

    LED代码
    LED闪烁完整代码 2017.2.20

--- Project/Objects/main.d
+++ Project/Objects/main.d
@@ -5,4 +5,3 @@
 .\objects\main.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmInstr.h
 .\objects\main.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmFunc.h
 .\objects\main.o: ..\CMSIS\system_stm32f10x.h
-.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
--- User/src/main.c
+++ User/src/main.c
@@ -1,5 +1,4 @@
 #include "stm32f10x.h"
-#include "stdio.h"
 
 void delay(u32 time){
 		while(time--);
@@ -7,16 +6,18 @@ void delay(u32 time){
 int main()
 	{
 		//打开时钟
-		RCC -> APB2ENR |= 1<<3;
+		RCC -> APB2ENR |= (1<<3);
 		//配置GPIO口PB5为输出模式
+		//清除PB5的初始值
 		GPIOB->CRL &= ~(0xf<<20);
-		GPIOB->CRL |= 3<<30;
+		//配置PB5为 推挽输出
+		GPIOB->CRL |= (0X3<<20);
 		//配置端口输出寄存器
 		//点亮LED灯
 		while(1){
-		GPIOB->ODR |= 1<<5;
+		GPIOB->ODR |= (1<<5);
 		delay(500000);
-		GPIOB->ODR |= ~(1<<5);
+		GPIOB->ODR &= ~(1<<5);
 		delay(500000);
 		}
 	}
