1d07f8dffe26e9af5325e2ba09f82ca4bbad9391
MiChong <1564666023@qq.com>
2017-02-20 17:35:47 +0800

    LED_Code
    LED灯的闪烁

--- User/src/main.c
+++ User/src/main.c
@@ -1,6 +1,22 @@
 #include "stm32f10x.h"
 #include "stdio.h"
+
+void delay(u32 time){
+		while(time--);
+	}
 int main()
-{
-	printf("hello world");
-}
+	{
+		//打开时钟
+		RCC -> APB2ENR |= 1<<3;
+		//配置GPIO口PB5为输出模式
+		GPIOB->CRL &= ~(0xf<<20);
+		GPIOB->CRL |= 3<<30;
+		//配置端口输出寄存器
+		//点亮LED灯
+		while(1){
+		GPIOB->ODR |= 1<<5;
+		delay(500000);
+		GPIOB->ODR |= ~(1<<5);
+		delay(500000);
+		}
+	}
